c4ddb6a820e2c7a9e51af3be93cd774a753b69cb
이찬호 <chanho@woowahan.com>
2020-12-23 23:37:51 +0900

    github page text

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9906b6a1930cc4f2f45b";
+/******/ 	var hotCurrentHash = "37bc9dac68fdfed28065";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ components/common/header.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`
+const Header = () => {
+  return (
+    <Container>
+      <div>header당 </div>
+    </Container>
+  )
+}
+
+export default Header
